8b3e00e000f856bb9eeb693e0fd49206eb947ead
Gregory Liras <gregliras@gmail.com>
2012-03-23 11:02:46 +0200

    removed xtra files

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/master/Workspace/my_chat
+# Install script for directory: /home/master/Repos/my_chat
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/master/Workspace/my_chat/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/master/Repos/my_chat/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/master/Workspace/my_chat/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/master/Repos/my_chat/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
